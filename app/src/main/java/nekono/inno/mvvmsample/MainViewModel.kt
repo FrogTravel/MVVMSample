f375ac71eeda507b9d2bfe672e51a3aa62ac8fe1
FrogTravel <e.levchenko@innopolis.ru>
2018-08-10 19:21:53 +0300

    Cleared version

--- app/src/main/java/nekono/inno/mvvmsample/MainViewModel.kt
+++ app/src/main/java/nekono/inno/mvvmsample/MainViewModel.kt
@@ -1,14 +1,12 @@
 package nekono.inno.mvvmsample
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
 
 class MainViewModel: ViewModel(){
     val stroke = MutableLiveData<String>()
 
-    val enableButton = MutableLiveData<Boolean>()
-        get(){
-            field.value = (stroke.value ?: "").length > 5
-            return field
-        }
+    val enableButton: LiveData<Boolean> = Transformations.map(stroke) {  it.length > 5 }
 }
